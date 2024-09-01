b2bebc41607c8ea21756f807d7e355afd22a5d7d
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2024-08-31 23:36:57 -0700

    cart sec

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Home from "./pages/Home"
+import Carts from "./components/Carts"
 export default function App() {
   return (
     <>
--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import MainSection from "../components/MainSection";
+import Carts from "../components/Carts";
 function Home(){
     return(
 
 <>
 <Navbar/>
-<div className="px-4 lg:px-5 xl:px-14  lg:space-x-2">
+<div className="px-4 lg:px-5 xl:px-14  ">
 <MainSection/>
+  <Carts/>
+
 </div>
 </>
     )
